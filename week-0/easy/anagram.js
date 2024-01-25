538ee804df423995a5f48af934a4352dbc16cc35
Sargun Singh Sethi <100695155+SargunSinghSethi@users.noreply.github.com>
2024-01-25 22:56:22 +0530

    Week 0 Assignment
    It contains question related to JavaScript given in the week 0 assignment  of 100devsx 0-100 cohort by harkirat singh

---
+++ week-0/easy/anagram.js
@@ -0,0 +1,30 @@
+/*
+  Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+  What's Anagram?
+  - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
+*/
+
+function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) return false;
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  let charCountMap = new Map();
+  for (let char of str1) {
+    charCountMap.set(char, (charCountMap.get(char) || 0) + 1);
+  }
+  for (let char of str2) {
+    if (charCountMap.has(char)) {
+      charCountMap.set(char, charCountMap.get(char) - 1);
+    } else {
+      return false;
+    }
+  }
+  for (let count of charCountMap.values()) {
+    if (count !== 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+module.exports = isAnagram;
